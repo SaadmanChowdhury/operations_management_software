60429ece3c07bfedf83f26426d20c9ae9d528650
Sofia Neherin <sofia.neherin@gmail.com>
2020-10-13 02:22:53 +0600

    created login UI

---
+++ resources/views/login.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login System</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="bg-image">
+		<div class="overlay"></div>
+		<div class="section-body" id="page-top">
+			<div class="container-fluid">
+				<div class="page-header">
+					<div>
+						<h1> 実績管理システム</h1>
+						
+					</div><!--floatleft-->
+					
+					
+				</div><!--page-header-->
+				
+			</div><!--container-fluid-->
+		</div><!--sectionbody-->
+		<div class="login-body box-shadow">
+			<div class="container">
+				<div class="profile-icon">
+					<img src="img/pro_icon.png" alt="profile-icon">
+				</div>
+				<div class="row p-r">
+					<h3>LOGIN</h3>
+				</div>
+				<div class="login-form">
+					<form>
+						<div class="row">
+							<p style="display: block;width: 100%">EMAIL</p>
+						</div>
+						<div class="input" style="width: 100%;display: block;">
+								<input type="text" name="email" placeholder="example@example.co.jp">
+						</div>
+						<div class="row">
+							<p style="display: block;width: 100%">PASSWORD</p>
+						</div>
+						<div class="input" style="width: 100%;display: block;">
+							<input type="password" name="password" placeholder="your password">
+						</div>
+					</form>
+				</div>
+				<div class="center">
+					<input class="btn btn-orange" type="submit" value="ロギング">
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</body>
+</html>
