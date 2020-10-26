c8557691e3502719557eebeff34e9d1168c3b9bd
Sofia Neherin <sofia.neherin@gmail.com>
2020-10-26 15:35:19 +0600

    Added the JS func again

--- public/js/main.js
+++ public/js/main.js
@@ -132,4 +132,69 @@ function normalSideBar(sidebar) {
     }, 410);
 }
 
+////====USER-LIST====////
 
+
+const pos=document.querySelector('.userlist-nav');
+const staffList=document.querySelectorAll('.staffs .card');
+var item=document.querySelectorAll('.pos');
+
+pos.addEventListener("click",filterPos);
+
+function filterPos(e){
+    e.preventDefault();
+    console.log(e.target.innerText);
+    switch(e.target.innerText)
+    {
+        case "全て":
+        {
+            for(i=0;i<item.length;i++)
+            {
+                staffList[i].style.display="flex";
+            }   
+            break;
+        }
+        case "PM":
+        {
+            for(i=0;i<item.length;i++)
+            {
+                if(item[i].innerText=="PM")
+                {
+                    staffList[i].style.display="flex";
+                }
+                else{
+                    staffList[i].style.display="none";
+                }
+            }
+            break;
+        }
+        case "SE":
+        {
+            for(i=0;i<item.length;i++)
+            {
+                if(item[i].innerText=="SE")
+                {
+                    staffList[i].style.display="flex";
+                }
+                else{
+                    staffList[i].style.display="none";
+                }
+            }
+            break;
+        }
+        case "PG":
+        {
+            for(i=0;i<item.length;i++)
+            {
+                if(item[i].innerText=="PG")
+                {
+                    staffList[i].style.display="flex";
+                }
+                else{
+                    staffList[i].style.display="none";
+                }
+            }
+            break;
+        }
+    }
+}
