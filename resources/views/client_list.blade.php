dfd24024855463d5ac5a842992e7a29972271582
Sofia Neherin <sofia.neherin@gmail.com>
2020-10-26 23:02:16 +0600

    changed Stylesheet

--- resources/views/client_list.blade.php
+++ resources/views/client_list.blade.php
@@ -2,10 +2,10 @@
 <div class="user-list container-fluid">
     <div class="p-r row">
         <div class="page-title float-left">
-            <h2 style="color: black;margin-left:21px">ユーザー一覧</h2>
+            <h2 style="color: black;margin-left:21px">お客様一覧</h2>
         </div>
         <div class="btn-holder float-right">
-            <a href="" class="register-btn btn-orange"><span class="fa fa-plus"></span> Register</a>
+            <a href="" class="register-btn btn-orange"><span class="fa fa-plus"></span> 新 規 追 加</a>
         </div>
     </div>
     <div class="d-flex">
@@ -13,11 +13,11 @@
         <div class="row row-content">
             <div class="content-width">
 
-                <div id="table-nav" class="primary">
+                <div id="table-nav" class="midori">
                     <div class="flex-col">
                         <ul class="display list-unstyled">
                             <li> コード <span class="fa fa-caret-down"></span></li>
-                            <li>氏名 <span class="fa fa-caret-down"></span></li>
+                            <li>会社名 <span class="fa fa-caret-down"></span></li>
                             <li>責任者 <span class="fa fa-caret-down"></span></li>
                             <li>受注顧合計 <span class="fa fa-caret-down"></span></li>
                             <li>実績粗利 <span class="fa fa-caret-down"></span></li>
@@ -29,17 +29,47 @@
 
                
 
-                <div class="staffs">
+                <div class="clients">
                     
                         <div class="card"">
                             <div class="card-header">
                                 <a>
                                     <div class="display list-unstyled">
                                         <li>001</li>
-                                        <li><img src="img/pro_icon.png" class="smallpic">
-                                            <div class="user-name">GTM</div>
-                                        </li>
-                                        <li>Sofia</li>
+                                        <li>GT宮崎</li>
+                                        <li><img src="img/pro_icon.png" class="smallpic"><div class="user-name">ソフィア</div></li>
+                                        <li class="pos">100000</li>
+                                        <li>Y50,00000</li>
+                                        <li><span><img src="img/edit.png" alt=""></span></li>
+
+                                    </div>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="card"">
+                            <div class="card-header">
+                                <a>
+                                    <div class="display list-unstyled">
+                                        <li>002</li>
+                                        <li>GT宮崎</li>
+                                        <li><img src="img/pro_icon.png" class="smallpic"><div class="user-name">ソフィア</div></li>
+                                        <li class="pos">100000</li>
+                                        <li>Y50,00000</li>
+                                        <li><span><img src="img/edit.png" alt=""></span></li>
+
+                                    </div>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="card"">
+                            <div class="card-header">
+                                <a>
+                                    <div class="display list-unstyled">
+                                        <li>003</li>
+                                        <li>GT宮崎</li>
+                                        <li><img src="img/pro_icon.png" class="smallpic"><div class="user-name">ソフィア</div></li>
                                         <li class="pos">100000</li>
                                         <li>Y50,00000</li>
                                         <li><span><img src="img/edit.png" alt=""></span></li>
