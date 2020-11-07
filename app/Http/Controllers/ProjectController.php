78c3b61f37ee30e6dfc6adbb95f453a7429d01bf
Utshab Roy <utshab.roy@gmail.com>
2020-11-07 22:16:32 +0600

    project list and create implemented

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Utilities\JSONHandler;
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        $project = new Project();
+        $list = $project->readProjectList();
+        return $list;
+        $viewParams["list"] = $list;
+        return view('project_list', ['projects' => $list]);
+    }
+
+    public function getCreateView()
+    {
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        return view('project.create');
+    }
+
+    public function createProject(Request $request)
+    {
+        $loggedUser = auth()->user();
+
+        if ($loggedUser->user_authority == config('User_authority.システム管理者')) {
+            $project = new Project();
+            $project->createProject($request);
+            return JSONHandler::emptySuccessfulJSONPackage();
+        }
+        return JSONHandler::errorJSONPackage("UNAUTHORIZED_ACTION");
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'project_id';
+
+    protected $fillable = [
+        'project_name',
+        'customer_id',
+        'manager_id',
+    ];
+
+    public function readProjectList()
+    {
+        $project = Project::select([
+            'project_id',
+            'project_name',
+        ])->get();
+
+        return $project;
+    }
+
+    public function createProject($request)
+    {
+        $validatedData = $request->validate([
+            'project_name' => 'required',
+            'customer_id' => 'required',
+            'manager_id' => 'required',
+        ]);
+
+        //saving new record
+        Project::create($validatedData);
+    }
 }
