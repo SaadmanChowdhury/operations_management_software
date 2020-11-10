dc05fbb8ecf7cb4795dba57c41b89a72505b16d1
Sofia Neherin <sofia.neherin@gmail.com>
2020-11-11 00:17:08 +0600

    project init

---
+++ public/js/project.js
@@ -0,0 +1,81 @@
+var coll1 = document.getElementById("row1head");
+var content1 = document.getElementById("row1");
+var content2 = document.getElementById("row2");
+var content3 = document.getElementById("row3");
+var plusbtn= document.getElementsByClassName("add");
+var view=window.matchMedia("(min-width: 1400px)");
+var h=50;
+function myFunction(view){
+  if(view.matches)
+  {
+    var pos=document.getElementsByClassName('table-right')[1].getBoundingClientRect();
+    plusbtn[1].style.marginTop=`${pos.bottom-pos.top-50}px`;
+  }
+  else{
+   var pos=document.getElementsByClassName('table-right')[1].getBoundingClientRect();
+    plusbtn[1].style.marginTop=`${pos.bottom-pos.top-30}px`;
+  }
+}
+myFunction(view);
+view.addEventListener("fullscreen",myFunction);
+coll1.addEventListener("click", function() {
+
+
+    if (content1.style.display === "block") {
+      content1.style.display = "none";
+    } else {
+      content2.style.display = "none";
+      content1.style.display = "block";
+      content3.style.display = "none";
+    }
+    
+    var pos=document.getElementsByClassName('table-right')[0].getBoundingClientRect();
+    var height=document.getElementsByTagName('tbody')[1].offsetHeight;
+    console.log(h);
+    // var pos2= plusbtn[0].getBoundingClientRect();
+    // console.log(pos.bottom-pos.top);
+    // console.log(plusbtn[0]);
+    plusbtn[0].style.marginTop=`${pos.bottom-pos.top-h}px`;
+    //console.log(pos2.top);
+    console.log(plusbtn[0].style.marginTop);
+
+
+
+});
+
+
+var coll2 = document.getElementById("row2head");				
+coll2.addEventListener("click", function() {
+
+
+
+    if (content2.style.display === "block") {
+      content2.style.display = "none";
+    } else {
+      content2.style.display = "block";
+      content1.style.display = "none";
+      content3.style.display = "none";
+    }
+    myFunction(view);
+
+
+});
+
+
+var coll3 = document.getElementById("row3head");				
+coll3.addEventListener("click", function() {
+
+
+
+    if (content3.style.display === "block") {
+      content3.style.display = "none";
+    } else {
+      content2.style.display = "none";
+      content1.style.display = "none";
+      content3.style.display = "block";
+    }
+
+    var pos=document.getElementsByClassName('table-right')[2].getBoundingClientRect();
+    plusbtn[2].style.marginTop=`${pos.bottom-pos.top-50}px`;
+
+});
--- resources/views/project_list.blade.php
+++ resources/views/project_list.blade.php
@@ -30,14 +30,21 @@
             <hr />
 
             {{-- ///====PROJECT-TABLE HEADER====/// --}}
-            <div id="table-nav" class="gray">
+            <div id="table-nav" class="orange">
                 <div class="flex-col">
                     <ul class="display list-unstyled">
-                        <li> コード</li>
-                        <li> コード</li>
-                        <li> コード</li>
-                        <li> コード</li>
-                        <li> コード</li>
+                        <li> 案件名</li>
+                        <li> 顧客</li>
+                        <li> 担当</li>
+                        <li>作業工程</li>
+                        <li> 見込</li>
+                        <li> 状況</li>
+                        <li> 受注月</li>
+                        <li> 検収月</li>
+                        <li> 売上高</li>
+                        <li> 売上原価</li>
+                        <li>売上総利益</li>
+                        <li> 利益率</li>
                     </ul>
                 </div>
             </div>
@@ -48,11 +55,286 @@
             <div class="project table-body">
 
                 <div class="card" id="project-row-">
-                    <div class=" card-header">
+                    <div class="card-header" id="row1head">
                         <div class="display list-unstyled">
-                            <li>準備中</li>
+                            <li>Project 1</li>
+                            <li>トゥエンティフォーセブン</li>
+                            <li>maruta</li>
+                            <li>Design</li>
+                            <li>A</li>
+                            <li>見積中</li>
+                            <li>2001</li>
+                            <li>2004</li>
+                            <li>12000</li>
+                            <li>13000</li>
+                            <li>40000</li>
+                            <li>40%</li>
                         </div>
                     </div>
+                    <div class="collapse show" id="row1" >
+                        <div class="card-body row">
+                            <div class="table-left">
+                                <table>
+                                  
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                              </table>
+                            </div>
+                            <div class="add">
+                                <button class="btn round-btn primary"><span class="fa fa-plus"></span></button>
+                            </div>
+                          <div class="table-right">
+                              <table>
+                                  <tr>
+                                      <th>Member</th>
+                                      <th>Ab</th>
+                                      <th>202001</th>
+                                      <th>202002</th>
+                                      <th>202003</th>
+                                      <th>202004</th>
+                                      <th>202005</th>
+                                      <th>202006</th>
+                                      <th>202001</th>
+                                      <th>202002</th>
+                                      <th>202003</th>
+                                      <th>202004</th>
+                                      <th>202005</th>
+                                      <th>202006</th>
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                  </tr>
+                              </table>
+                          </div>
+                          <div class="action">
+                              <ul class="list-unstyled">
+                                  <li class="list"><button class="btn round-btn danger"><span class="fa fa-trash"></span></button></li>
+                                  <li class="list"><button class="btn round-btn success"><span class="fa fa-clone"></span></button></li>
+                                  <li class="list"><button  class="btn round-btn primary"><span class="fa fa-save"></span></button></li>
+
+                              </ul>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                <div class="card" id="project-row-">
+                    <div class="card-header" id="row2head">
+                        <div class="display list-unstyled">
+                            <li>Project 1</li>
+                            <li>トゥエンティフォーセブン</li>
+                            <li>maruta</li>
+                            <li>Design</li>
+                            <li>A</li>
+                            <li>見積中</li>
+                            <li>2001</li>
+                            <li>2004</li>
+                            <li>12000</li>
+                            <li>13000</li>
+                            <li>40000</li>
+                            <li>40%</li>
+                        </div>
+                    </div>
+                    <div class="collapse show" id="row2" >
+                        <div class="card-body row">
+                            <div class="table-left">
+                                <table>
+                                  
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                                  <tr>
+                                      <td>ABC</td>
+                                      <td>Y100000</td>
+                                  </tr>
+                              </table>
+                            </div>
+                            <div class="add">
+                                <button class="btn round-btn primary"><span class="fa fa-plus"></span></button>
+                            </div>
+                          <div class="table-right">
+                              <table>
+                                  <tr>
+                                      <th>Member</th>
+                                      <th>Ab</th>
+                                      <th>202001</th>
+                                      <th>202002</th>
+                                      <th>202003</th>
+                                      <th>202004</th>
+                                      <th>202005</th>
+                                      <th>202006</th>
+                                      <th>202001</th>
+                                      <th>202002</th>
+                                      <th>202003</th>
+                                      <th>202004</th>
+                                      <th>202005</th>
+                                      <th>202006</th>
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      
+                                  </tr>
+                                  <tr>
+                                      <td><img src="img/pro_icon.png">Sofia</td>
+                                      <td>none</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                      <td>5.00</td>
+                                  </tr>
+                              </table>
+                          </div>
+                          <div class="action">
+                              <ul class="list-unstyled">
+                                  <li class="list"><button class="btn round-btn danger"><span class="fa fa-trash"></span></button></li>
+                                  <li class="list"><button class="btn round-btn success"><span class="fa fa-clone"></span></button></li>
+                                  <li class="list"><button  class="btn round-btn primary"><span class="fa fa-save"></span></button></li>
+
+                              </ul>
+                          </div>
+                        </div>
+                      </div>
                 </div>
             </div>
         </div>
