69b2c3ab27836acb864d401cc6c731b168bb8ee2
Sofia Neherin <sofia.neherin@gmail.com>
2020-12-16 09:45:34 +0600

    updated date and pro pic

--- public/js/user.js
+++ public/js/user.js
@@ -1,79 +1,5 @@
 ////====USER-LIST====////
 
-
-var pos = document.querySelector('.userlist-nav');
-var staffList = document.querySelectorAll('.staffs .card');
-var item = document.querySelectorAll('.pos');
-
-pos.addEventListener("click", filterPos);
-
-function filterPos(e) {
-    e.preventDefault();
-    console.log(e.target.innerText);
-    switch (e.target.innerText) {
-        case "全て":
-            {
-                for (i = 0; i < item.length; i++) {
-                    showCard(staffList[i])
-                }
-                break;
-            }
-        case "PM":
-            {
-                for (i = 0; i < item.length; i++) {
-                    if (item[i].innerText == "PM") {
-                        showCard(staffList[i])
-                    }
-                    else {
-                        hideCard(staffList[i])
-                    }
-                }
-                break;
-            }
-        case "SE":
-            {
-                for (i = 0; i < item.length; i++) {
-                    if (item[i].innerText == "SE") {
-                        showCard(staffList[i])
-                    }
-                    else {
-                        hideCard(staffList[i])
-                    }
-                }
-                break;
-            }
-        case "PG":
-            {
-                for (i = 0; i < item.length; i++) {
-                    if (item[i].innerText == "PG") {
-                        showCard(staffList[i])
-                    }
-                    else {
-                        hideCard(staffList[i])
-                    }
-                }
-                break;
-            }
-        case "PL":
-            {
-                for (i = 0; i < item.length; i++) {
-                    if (item[i].innerText == "PL") {
-                        showCard(staffList[i])
-                    }
-                    else {
-                        hideCard(staffList[i])
-                    }
-                }
-                break;
-            }
-    }
-}
-function numberWithCommas(x) {
-    var z = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    console.log("hey");
-    document.getElementsByClassName('salary').innerText = z;
-}
-
 function fetchUserList_AJAX() {
     $.ajax({
         type: "post",
@@ -151,12 +77,40 @@ function renderHTML(response) {
                 loc='宮崎';
                 break;
         }
+        // DATE
+        let today = new Date();
+        let date=new Date(row.admissionDay);
+        var unit='年';
+        var time_diff=Math.floor((today.getTime()-date.getTime())/1000/3600/24/365);
+        if(time_diff==0)
+        {
+            time_diff=Math.floor((today.getTime()-date.getTime())/1000/3600/24/30);
+            unit='月';
+
+        }
+        // console.log(time_diff);
+
+        //GENDER
+        var gender;
+        switch(row.gender)
+        {
+            case 0:
+                gender='./img/pro_icon.png';
+                break;
+            case 1:
+                gender='./img/pro_icon3.png';
+                break;
+        }
+        console.log(gender);
+
+
+
         rowHtml=`<li>${row.userID}</li>`+
-                `<li><img src="./img/pro_icon.png" class="smallpic">
+                `<li><img src="${gender}" class="smallpic">
                 <div class="user-name">${row.username}</div></li>`+
                 `<li class="user-location">${loc}</li>`+
                 `<li class="pos pos-${pos}">${pos}</li>`+
-                `<li>${row.admissionDay}</li>`+
+                `<li>${time_diff}${unit}</li>`+
                 `<li>${row.unitPrice}</li>`;
         display.innerHTML+=rowHtml;
         // switch(row.position)
@@ -196,3 +150,76 @@ function renderHTML(response) {
 
 }
 document.addEventListener("DOMContentLoaded",()=>{fetchUserList_AJAX()});
+
+var pos = document.querySelector('.userlist-nav');
+var staffList = document.querySelectorAll('.staffs .card');
+var item = document.querySelectorAll('.pos');
+
+pos.addEventListener("click", filterPos);
+
+function filterPos(e) {
+    e.preventDefault();
+    console.log(e.target.innerText);
+    switch (e.target.innerText) {
+        case "全て":
+            {
+                for (i = 0; i < item.length; i++) {
+                    showCard(staffList[i])
+                }
+                break;
+            }
+        case "PM":
+            {
+                for (i = 0; i < item.length; i++) {
+                    if (item[i].innerText == "PM") {
+                        showCard(staffList[i])
+                    }
+                    else {
+                        hideCard(staffList[i])
+                    }
+                }
+                break;
+            }
+        case "SE":
+            {
+                for (i = 0; i < item.length; i++) {
+                    if (item[i].innerText == "SE") {
+                        showCard(staffList[i])
+                    }
+                    else {
+                        hideCard(staffList[i])
+                    }
+                }
+                break;
+            }
+        case "PG":
+            {
+                for (i = 0; i < item.length; i++) {
+                    if (item[i].innerText == "PG") {
+                        showCard(staffList[i])
+                    }
+                    else {
+                        hideCard(staffList[i])
+                    }
+                }
+                break;
+            }
+        case "PL":
+            {
+                for (i = 0; i < item.length; i++) {
+                    if (item[i].innerText == "PL") {
+                        showCard(staffList[i])
+                    }
+                    else {
+                        hideCard(staffList[i])
+                    }
+                }
+                break;
+            }
+    }
+}
+function numberWithCommas(x) {
+    var z = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    console.log("hey");
+    document.getElementsByClassName('salary').innerText = z;
+}
