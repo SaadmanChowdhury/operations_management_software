0271bb99ee9e7368b839937cad55767f01f7e8d6
sigma-complex <49648147+sigma-complex@users.noreply.github.com>
2021-04-30 12:53:58 +0900

    Design and Code refactored

--- public/js/user.js
+++ public/js/user.js
@@ -89,14 +89,22 @@ function renderHTML(response) {
         }
 
         var editableButtonString =`<li><div class="edit" onclick="userEditModalHandler(${row.userID})"><span style="font-size: 11px; margin:6px;width:auto" class="fa fa-pencil"></span>編集</div></li>` ;
+        var unitPriceString =  ``;
 
-        if(isUserModalEditable(row.userID)){
-
-        }
+        if(isUserModalEditable(row.userID)){}
         else{
             editableButtonString =`<li ></li>` ;
-
         }
+
+        if(isSystemAdmin()){
+            unitPriceString =  `<li>${unitPrice}</li>`;
+        }
+        
+        if(isGeneralUser()){
+            unitPriceString =  ``;
+        }
+
+
 
         rowHtml = `<div class="card _user" id="user-row-${row.userID}">` +
             `<div class="card-header">` +
@@ -107,7 +115,7 @@ function renderHTML(response) {
             `<li class="user-location">${loc}</li>` +
             `<li><div class="pos pos-${pos}">${pos}</div></li>` +
             `<li>${time_diff}${unit}</li>` +
-            `<li>${unitPrice}</li>` +
+            unitPriceString +
             editableButtonString+
             `</div></div></div>`;
 
@@ -115,25 +123,30 @@ function renderHTML(response) {
         staffList = document.querySelectorAll('.staffs .card');
         item = document.querySelectorAll('.pos');
     });
-
 }
 
+function isGeneralUser(){
+    var currentUserAuthority=document.getElementById("user-authority");
+    return currentUserAuthority.value=="一般ユーザー"?true:false;
+}
+
+function isSystemAdmin(){
+    var currentUserAuthority=document.getElementById("user-authority");
+    return currentUserAuthority.value=="システム管理者"?true:false;
+}
+
+function isCurrentUser(userId){
+    var currentUserId= document.getElementById("logged-in-id");
+    return userId==currentUserId.value?true:false;
+}
 
 function isUserModalEditable(userId){
-
-    var currentUserId= document.getElementById("logged-in-id");
-    var currentUserAuthority=document.getElementById("user-authority");
-
-    if(currentUserAuthority.value=="システム管理者")
+    if(isSystemAdmin())
         return true;
-    else if(currentUserAuthority.value=="一般ユーザー"){
-            if(userId==currentUserId.value){
-                return true;
-            }
-        }
+    else if(isGeneralUser() && isCurrentUser(userId))
+        return true;
     else
-        return false
-
+        return false;
 }
 
 
