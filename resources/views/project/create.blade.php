2b8c9675050017919b5b0f536c4a0e25416ce17e
sigma-complex <49648147+sigma-complex@users.noreply.github.com>
2021-05-21 12:03:13 +0900

    Fix added after editing project after insertion

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -178,8 +178,8 @@ function createProject() {
         cache: false,
         success: function(response) {
             if (response["resultStatus"]["isSuccess"]) {
-                // updateProjectTable(modalData);
-
+                
+                modalData["projectID"] = response["resultData"]["projectID"];
                 let row = $("#accordian");
                 row.prepend( new ProjectListRenderer().renderHTMLProjectList(modalData));
                 closeModal('project-create-modal');
