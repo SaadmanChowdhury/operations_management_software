9dddb1d8c3b7a8dfbbda29b7d63b63f217872e84
sigma-complex <49648147+sigma-complex@users.noreply.github.com>
2021-06-17 16:49:26 +0900

    New user added from Acro

--- database/seeders/UserOneSeeder.php
+++ database/seeders/UserOneSeeder.php
@@ -18,8 +18,8 @@ class UserOneSeeder extends Seeder
 
         $data = [
             [
-                'name' => '中村',
-                'email' => 'nakamura@gtmi.co.jp',
+                'name' => '斉藤晃央',
+                'email' => 'akio.saito@acro-system.com',
                 'email_verified_at' => now(),
                 'password' => bcrypt('password'), // password
                 'gender' => 0, //male
@@ -28,7 +28,7 @@ class UserOneSeeder extends Seeder
                 'position' => 'PM', //PM
                 'admission_day' => '2019-01-01',
                 'exit_day' => null,
-                'unit_price' => 400000,
+                'unit_price' => 0,
                 'user_authority' => 'システム管理者',
                 'resign_day' => null,
             ],
